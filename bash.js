890608a803ebc514f0d94970bd475f3c497e6fd7
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-03-04 13:14:34 -0600

    fifth commit

--- bash.js
+++ bash.js
@@ -1,24 +1,21 @@
-
 const prompt = '\nprompt > ';
 process.stdout.write(prompt);
 
 const pwd = require('./pwd');
 const ls = require('./ls');
-const cat = require('./cat')
+const cat = require('./cat');
 
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim()
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
   const cmdArray = cmd.split(' ');
 
   if (cmdArray[0] === 'pwd') {
     pwd();
   }
-  if (cmdArray[0] === 'ls'){
+  if (cmdArray[0] === 'ls') {
     ls();
   }
-  if (cmdArray[0] === 'cat'){
-    cat(cmdArray[1])
+  if (cmdArray[0] === 'cat') {
+    cat(cmdArray[1]);
   }
-
 });
-
--- cat.js
+++ cat.js
@@ -3,7 +3,6 @@ const fs = require('fs');
 function cat (filePath) {
 
   fs.readFile(`${filePath}`, 'utf8', (err, data) => {
-    console.log(data)
     if (err) {throw err; }
     else {
       console.log(data)
